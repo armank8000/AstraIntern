5626a6018bebe78b8b3b739b76b2fdd6379af598
Arman kumar <140732286+armank8000@users.noreply.github.com>
2026-05-28 14:39:55 +0000

    initial_commit

---
+++ enum_sub.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+Target=$1
+Timestamp=$(date +%Y%m%d_%H%M%S)
+Output_Dir="sub_${TIMESTAMP}.txt"
+
+echo "Target provided ${Target}"
+
+echo "Performing subdomain enumeration......."
+
+
+
+
+subfinder -d $Target > $Output_Dir
+subfinder -d $Target -v >> $Output_Dir
+echo "subfinder scan completed..."
+
+amass enum -passive -d $Target >> $Output_Dir
+amass enum -d $Target >> $Output_Dir
+echo "amass scan completed...."
+
+assetfinder --subs-only $Target >> $Output_Dir
+echo "assetfinder scan completed...."
+
+cat $Output_Dir | sort -u 
